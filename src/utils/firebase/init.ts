5ca1944f701d5ce0fdd72327f491204551d9e214
Mario Galeno <galeno.mario@gmail.com>
2022-07-18 00:44:47 -0500

    initialize firebase auth

--- src/utils/firebase/init.ts
+++ src/utils/firebase/init.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,7 +19,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const initFirebaseApp = () => {
-  const app = initializeApp(firebaseConfig, "track-da-baby");
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const analytics = getAnalytics(app);
+  const firApp = initializeApp(firebaseConfig, "track-da-baby");
+  const analytics = getAnalytics(firApp);
+  const auth = getAuth(firApp);
+
+  return { firApp, analytics, auth };
 };
