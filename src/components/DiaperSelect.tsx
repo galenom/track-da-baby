2aa230bbd9c4d3607f757bb4f74fe3f8774a363b
Mario Galeno <galeno.mario@gmail.com>
2022-07-19 15:50:44 -0500

    Store lastdiaper in component state

--- src/components/DiaperSelect.tsx
+++ src/components/DiaperSelect.tsx
@@ -2,21 +2,27 @@ import IconButton from "@mui/material/IconButton";
 import { Typography } from "@mui/material";
 import ButtonGroup from "@mui/material/ButtonGroup";
 // import DashboardCustomizeIcon from "@mui/icons-material/DashboardCustomize";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./DiaperSelect.css";
 import { DiaperState } from "../utils/diaper/types";
 import { useAuthorizedUser } from "../utils/AuthContext";
-import { logDiaperState } from "../utils/firebase/database";
+import { getLastDiaper, logDiaperState } from "../utils/firebase/database";
 
 interface DiaperSelectProps {}
 
 export const DiaperSelect = (_: DiaperSelectProps) => {
   const user = useAuthorizedUser();
+  const [lastDiaper, setLastDiaper] = useState<any>(null);
+
+  useEffect(() => {
+    getLastDiaper(user!.uid).then((diaper) => {
+      setLastDiaper(diaper);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const handleClick = (diaperState: DiaperState) => {
-    if (user) {
-      logDiaperState(user.uid, diaperState);
-    }
+    logDiaperState(user!.uid, diaperState);
   };
 
   return (
